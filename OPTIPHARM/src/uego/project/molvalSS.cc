235997339b5b414aafc4c1f755f7816ec4a3f72d
Savíns Puertas Martín <savinspm@ual.es>
2025-07-07 20:07:31 +0200

    Add files via upload

---
+++ OPTIPHARM/src/uego/project/molvalSS.cc
@@ -0,0 +1,124 @@
+#include "uego.h"
+#include "time.h"
+// #include "internal.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include "Tanimoto.h"
+#include <iomanip>
+////////////////////////////////////////////////////////////
+// $Id: testval.cc,v 2.6 1998/03/29 10:40:32 jelasity Exp $
+// testval.cc
+// definition of NDimRealElement::Value()
+// objective function library for n dimensional real spaces
+// this version was originaly created for experimental tests
+////////////////////////////////////////////////////////////
+// modification history:
+////////////////////////////////////////////////////////////
+
+//-------------------------------------------------------------------------
+#include "VolumeOverlap.h"
+#include "MoveAndRotate.h"
+#include "Tanimoto.h"
+#include <iostream>
+
+double NDimMolElement::Value()
+{
+    // time_t t1,t2;
+
+    // t1=time(NULL);
+    if (x[1] == x[4] && x[2] == x[5] && x[3] == x[6])
+    {
+        return 0;
+    }
+    // NUEVO
+    // double * newAtomsRotated =
+    //		new double[INI.getMolVariable()->atoms.size() * 3];
+
+    /*MoveAndRotate::RotateMolAccording1Axis(INI.getMolVariable()->getAtomsXYZ(),
+     INI.getMolVariable()->atoms.size(), x[0], x[1], x[2], x[3], 0, 0,
+     0, newAtomsRotated);*/
+    double *newAtomsRotated =
+        &(INI.getMolVariable()->getAtomsXYZ()[INI.getMolVariable()->atoms.size() * 3]);
+    MoveAndRotate::PreciseRotateMolAccording1Axis(
+        INI.getMolVariable()->getAtomsXYZ(),
+        INI.getMolVariable()->atoms.size(), x[0], x[1], x[2], x[3], x[4],
+        x[5], x[6], newAtomsRotated);
+
+    MoveAndRotate::PreciseMolToNewPosition(newAtomsRotated,
+                                           INI.getMolVariable()->atoms.size(), x[7], x[8], x[9]);
+
+    /*
+     *
+     * ESTE METODO USA LOS PESOS DE LAS DOS MOLECULAS
+     *
+     *
+     */
+
+    double VAB = VolumeOverlap::preciseOverlapWEGA(
+        INI.getMolQuery()->getAtomsXYZ(),
+        INI.getMolQuery()->atoms.size(), INI.getMolQuery()->getWeightAtoms(),
+        INI.getMolQuery()->getRadiusAtoms(), newAtomsRotated,
+        INI.getMolVariable()->atoms.size(),
+        INI.getMolVariable()->getWeightAtoms(),
+        INI.getMolVariable()->getRadiusAtoms(),
+        INI.getSameVanDerWaalsRadius());
+
+    double result = Tanimoto::calculateTanimotoGeneric(
+        INI.getMolQuery()->tanimoto,
+        INI.getMolVariable()->tanimoto, VAB);
+    // cout << INI.getMolQuery()->tanimoto <<", "<< INI.getMolVariable()->tanimoto<<", " << VAB<<", "<<result <<"\n";
+    // delete[] newAtomsRotated;
+
+    return result;
+};
+
+double NDimMolElement::PreciseValue()
+{
+    // time_t t1,t2;
+
+    // t1=time(NULL);
+    if (x[1] == x[4] && x[2] == x[5] && x[3] == x[6])
+    {
+        return 0;
+    }
+    // NUEVO
+    // double * newAtomsRotated =
+    //		new double[INI.getMolVariable()->atoms.size() * 3];
+
+    /*MoveAndRotate::RotateMolAccording1Axis(INI.getMolVariable()->getAtomsXYZ(),
+     INI.getMolVariable()->atoms.size(), x[0], x[1], x[2], x[3], 0, 0,
+     0, newAtomsRotated);*/
+    double *newAtomsRotated =
+        &(INI.getMolVariable()->getAtomsXYZ()[INI.getMolVariable()->atoms.size() * 3]);
+    MoveAndRotate::PreciseRotateMolAccording1Axis(
+        INI.getMolVariable()->getAtomsXYZ(),
+        INI.getMolVariable()->atoms.size(), x[0], x[1], x[2], x[3], x[4],
+        x[5], x[6], newAtomsRotated);
+
+    MoveAndRotate::PreciseMolToNewPosition(newAtomsRotated,
+                                           INI.getMolVariable()->atoms.size(), x[7], x[8], x[9]);
+
+    /*
+     *
+     * ESTE METODO USA LOS PESOS DE LAS DOS MOLECULAS
+     *
+     *
+     */
+
+    double VAB = VolumeOverlap::preciseOverlapWEGA(
+        INI.getMolQuery()->getAtomsXYZ(),
+        INI.getMolQuery()->atoms.size(), INI.getMolQuery()->getWeightAtoms(),
+        INI.getMolQuery()->getRadiusAtoms(), newAtomsRotated,
+        INI.getMolVariable()->atoms.size(),
+        INI.getMolVariable()->getWeightAtoms(),
+        INI.getMolVariable()->getRadiusAtoms(),
+        INI.getSameVanDerWaalsRadius());
+
+    double result = Tanimoto::calculateTanimotoGeneric(
+        INI.getMolQuery()->tanimoto,
+        INI.getMolVariable()->tanimoto, VAB);
+    // cout << INI.getMolQuery()->tanimoto <<", "<< INI.getMolVariable()->tanimoto<<", " << VAB<<", "<<result <<"\n";
+    // delete[] newAtomsRotated;
+
+    return result;
+};
